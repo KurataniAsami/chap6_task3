09df2561c88ddf56accb3edd6c244efce8d9a1e5
kurataniasami <kura56135613@gmail.com>
2025-12-03 20:47:40 +0900

    App.jsを変更

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getAllPosts = async () => {
+    const response = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts")
+    const data = await response.json();
+    setPosts(data.posts);
+  };
+  getAllPosts();
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          test <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <div className={styles["flex-row"]}>
+            <div>{post.createdAt}</div>
+            <div className={styles["category-tag"]}>{post.categories.join(', ')}</div>
+          </div>
+          <h3>{post.title}</h3>
+          <div className={styles["line-clamp"]}>
+            <div dangerouslySetInnerHTML={{ __html: post.content }} />
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 }
 

672415c6122e4cbee434f143673383f008947c3b
kurataniasami <kura56135613@gmail.com>
2025-12-05 11:33:28 +0900

    PostDetail.jsを作成

---
+++ src/Details/PostDetail.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+
+const PostDetail = () => {
+  const [post, setPost] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getAllPosts = async () => {
+    const response = await fetch(`https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${id}`)
+    const data = await response.json();
+    setPost(data.post);
+  };
+  getAllPosts();
+  }, [id]);
+
+  if (!post)
+    return
+      <div>Loading</div>
+  
+  return (
+    <div>
+      <h2>{post.title}</h2>
+      <div>{post.createdAt}</div>
+      <div>{post.categories}</div>
+      <div dangerouslySetInnerHTML={{ __html: post.content }} />
+    </div>
+  );
+}
+
+export default PostDetail

9b79e505038f2fd3572df23dbd3ad4ec8fe29cf9
kurataniasami <kura56135613@gmail.com>
2025-12-05 11:33:08 +0900

    App.jsを変更

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
-import { useEffect, useState } from 'react';
-import styles from './App.module.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './Home/Home';
+import PostDetail from './Details/PostDetail';
 
-function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const getAllPosts = async () => {
-    const response = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts")
-    const data = await response.json();
-    setPosts(data.posts);
-  };
-  getAllPosts();
-  }, []);
-  
+const App = () => {
   return (
-    <ul>
-      {posts.map((post) => (
-        <li key={post.id}>
-          <div className={styles["flex-row"]}>
-            <div>{post.createdAt}</div>
-            <div className={styles["category-tag"]}>{post.categories.join(', ')}</div>
-          </div>
-          <h3>{post.title}</h3>
-          <div className={styles["line-clamp"]}>
-            <div dangerouslySetInnerHTML={{ __html: post.content }} />
-          </div>
-        </li>
-      ))}
-    </ul>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/posts/:id' element={<PostDetail />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App

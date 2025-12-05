6c37512199419dec5186e35aad7ca298b45ba387
kurataniasami <kura56135613@gmail.com>
2025-12-05 11:33:49 +0900

    Home.jsを作成

---
+++ src/Home/Home.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import styles from './Home.module.css';
+
+function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getAllPosts = async () => {
+    const response = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts")
+    const data = await response.json();
+    setPosts(data.posts);
+  };
+  getAllPosts();
+  }, []);
+
+  if (!posts)
+    return
+      <div>読み込み中</div>
+  
+    return (
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={`/posts/${post.id}`} className={styles["postItem"]}>
+              <div className={styles["flex-row"]}>
+                <div>{post.createdAt}</div>
+                <div className={styles["category-tag"]}>{post.categories.join('')}</div>
+              </div>
+              <h3>{post.title}</h3>
+              <div className={styles["line-clamp"]}>
+                <div dangerouslySetInnerHTML={{ __html: post.content }} />
+              </div>
+            </ Link>
+          </li>
+        ))}
+      </ul>
+  );
+}
+
+export default Home
